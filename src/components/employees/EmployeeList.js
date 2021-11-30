3eecaf63a49c7b8aaf4180d1c29397870f838d99
keosha-w <keosha.watkins@asurion.com>
2021-11-30 14:30:06 -0600

    Changed the way that we delete and created a function that will re-render the page

--- src/components/employees/EmployeeList.js
+++ src/components/employees/EmployeeList.js
@@ -9,20 +9,25 @@ export default () => {
 
     useEffect(
         () => {
+            render()
+        }, []
+        )
+        
+    const render = () => {
             EmployeeRepository.getAll()
             .then(
                 (em) => {
                     setEmployees(em)
                 }
             )
-        }, []
-    )
 
+  }
+    
     return (
         <>
             <div className="employees">
                 {
-                    emps.map(a => <Employee key={a.id} employee={a} />)
+                    emps.map(a => <Employee key={a.id} employee={a} renderFunc={render}/>)
                 }
             </div>
         </>
