b3506a3e9fcef07fea14ddec47a21b50a1e4164f
Chris Mccrary <chris.mccrary@asurion.com>
2021-12-03 10:49:01 -0600

    moved employee fire buttom, enabled dismiss button function for current pets, and removed employeeId key from created animal

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Link, useHistory } from "react-router-dom"
 import AnimalRepository from "../../repositories/AnimalRepository";
 import useSimpleAuth from "../../hooks/ui/useSimpleAuth";
