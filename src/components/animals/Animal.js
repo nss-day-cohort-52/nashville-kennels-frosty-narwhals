4dafd7f086d37f3c8b3fc9d1a46738f36085ac7e
keosha-w <keosha.watkins@asurion.com>
2021-12-03 11:12:55 -0600

    animal caretakers to display

--- src/components/animals/Animal.js
+++ src/components/animals/Animal.js
@@ -6,9 +6,13 @@ import OwnerRepository from "../../repositories/OwnerRepository";
 import useSimpleAuth from "../../hooks/ui/useSimpleAuth";
 import useResourceResolver from "../../hooks/resource/useResourceResolver";
 import "./AnimalCard.css"
+import EmployeeRepository from "../../repositories/EmployeeRepository";
 
-export const Animal = ({ animal, syncAnimals,
-    showTreatmentHistory, owners }) => {
+export const Animal = ({ 
+    animal, 
+    syncAnimals,
+    showTreatmentHistory, 
+    owners }) => {
     const [detailsOpen, setDetailsOpen] = useState(false)
     const [isEmployee, setAuth] = useState(false)
     const [myOwners, setPeople] = useState([])
@@ -52,6 +56,11 @@ export const Animal = ({ animal, syncAnimals,
         }
     }, [animalId])
 
+
+
+
+   
+
     return (
         <>
             <li className={classes}>
@@ -84,7 +93,8 @@ export const Animal = ({ animal, syncAnimals,
                         <section>
                             <h6>Caretaker(s)</h6>
                             <span className="small">
-                                {currentAnimal?.animalOwners?.users?.name}
+
+                                {animal.animalCaretakers.map(care => care.user.name).join(", ")}
                             </span>
 
 
